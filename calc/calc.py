b88cbcd39e08eadc096549c694f873a5938ea02b
Chaemin-L <chaem12@kookmin.ac.kr>
2020-06-28 12:24:15 +0900

    Fixed Wrong File

--- calc/calc.py
+++ calc/calc.py
@@ -5,10 +5,11 @@ def application(environ, start_response):
 	d = parse_qs(environ['QUERY_STRING'])
 	first_num = d.get('first_num', [''])[0]
 	second_num = d.get('second_num', [''])[0]
-	sum, mul = 0, 0
-	first_num, second_num = int(first_num), int(second_num) 
-	sum = first_num + second_num
-	mul = first_num *second_num
+
+	if '' not in [first_num, second_num]:
+		first_num, second_num = int(first_num), int(second_num) 
+		sum = first_num + second_num
+		mul = first_num *second_num
 	response_body = html % {'sum':sum, 'mul':mul}
 	start_response('200 OK', [
 		('Content-Type', 'text/html'),
